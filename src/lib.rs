2b0e5d13773124a37ce300db5b359c53751360c4
Jeremiah Russell <jrussell@jerus.ie>
2021-09-29 17:50:04 +0100

    FIX: rustdoc in nightly not finding the examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(missing_docs)]
-#![cfg_attr(docsrs, warn(rustdoc::missing_doc_code_examples))]
+// #![cfg_attr(docsrs, warn(rustdoc::missing_doc_code_examples))]
 #![cfg_attr(docsrs, warn(rustdoc::invalid_codeblock_attributes))]
 #![allow(dead_code)]
 
